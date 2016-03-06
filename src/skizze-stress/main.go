5f2132b746859d48fb8646510216e0159c288ae8
Seif Lotfy <seif.lotfy@gmail.com>
2016-03-06 10:08:23 +0100

    update stress

--- src/skizze-stress/main.go
+++ src/skizze-stress/main.go
@@ -30,25 +30,27 @@ func main() {
 
 	end := time.Duration(0)
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	zipf := rand.NewZipf(r, 1.1, 1.1, 500000)
-	totalAdds := uint64(0)
-	for i := 0; i < 100; i++ {
-		word := words[i]
-		n := zipf.Uint64() + 1
-		fmt.Printf("%d Push: %s (%d times)\n", i, word, n)
-		totalAdds += n
-		fill := make([]string, n, n)
+	zipf := rand.NewZipf(r, 1.1, 1.1, uint64(len(words)-1))
+	totalAdds := 0
+	for i := 0; i < 100000; i++ {
+		fill := make([]string, 1000, 1000)
 		for j := 0; j < len(fill); j++ {
-			fill[j] = word
+			k := zipf.Uint64()
+			fill[j] = words[k]
 		}
+		totalAdds += len(fill)
+
 		t := time.Now()
 		if err := client.AddToDomain(domainName, fill...); err != nil {
 			fmt.Println(err)
 			return
 		}
 		end += time.Since(t)
+		if end.Seconds() > 0 {
+			fmt.Printf("Added %d values (%d unique) in %ds (avg. %d v/s)\n", totalAdds, len(words), int(end.Seconds()), totalAdds/int(end.Seconds()+1))
+		}
 	}
 
 	client.Close()
-	fmt.Printf("Added %d values (%d unique) in %ds\n", totalAdds, len(words), int(end.Seconds()))
+	fmt.Printf("Added %d values (%d unique) in %ds (avg. %d v/s)\n", totalAdds, len(words), int(end.Seconds()), totalAdds/int(end.Seconds()+1))
 }
